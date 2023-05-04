72b2087a78a6a757b7ad9b48e2a2e0c8caa551df
Karina Koldaeva <71209378+KarinaKold@users.noreply.github.com>
2023-05-04 22:20:17 +0300

    feat: create welcome page with routes, configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,17 @@
-import "sanitize.css";
-import "./App.css";
+import 'sanitize.css';
+import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import WelcomePage from './pages/WelcomePage';
+import NotFoundPage from './pages/NotFoundPage';
 
-export function App() {}
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const WelcomePage = () => {
+  return (
+    <>
+      <div>
+        <h2>WELCOME TO</h2>
+        <h1>GraphiQL App</h1>
+      </div>
+    </>
+  );
+};
+
+export default WelcomePage;
