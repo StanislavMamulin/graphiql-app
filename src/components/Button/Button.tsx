674aeb3d56f30a8f703af1dd6c658507d2aadb5f
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-06 11:16:29 +0500

    feat: add ability to add extra style for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,11 @@ import styles from './Button.module.css';
 type ButtonProps = {
   title: string;
   clickHandler: () => void;
+  extraClass?: string;
 };
 
-export const Button = ({ title, clickHandler }: ButtonProps): JSX.Element => (
-  <button onClick={clickHandler} className={styles.button}>
+export const Button = ({ title, clickHandler, extraClass }: ButtonProps): JSX.Element => (
+  <button onClick={clickHandler} className={styles.button + ' ' + extraClass}>
     {title}
   </button>
 );
