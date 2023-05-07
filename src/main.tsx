bc236c00032b76829c920857180c7cb33d633e10
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-07 21:51:46 +0500

    feat: add auth methods with firebase

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { I18nextProvider } from 'react-i18next';
 import { App } from './App.tsx';
 import i18n from './i18n';
 import { store } from './redux/store.ts';
+import './services/firebase/firebase.ts';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
