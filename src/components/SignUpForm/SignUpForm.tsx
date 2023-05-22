3c31265d1f148cdbab4ecee70516032794d9c31d
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-22 18:56:13 +0500

    feat: add error boundaries for needed components

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -10,6 +10,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import FormInput from '../FormInput/FormInput';
 import { Spinner } from '../../components/Spinner/Spinner';
 import { useTranslation } from 'react-i18next';
+import { ErrorBoundary } from '../../components/ErrorBoundary/ErrorBoundary';
 
 const SignUpForm: FC = () => {
   const { t } = useTranslation();
@@ -73,77 +74,79 @@ const SignUpForm: FC = () => {
       )}
     >
       {isSubmitting && <Spinner />}
-      <form
-        className={styles.signup_form.concat(' ', errors.form ? styles.hasError : '')}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        {
-          <span role="alert" className={styles.error}>
-            {errors.form?.message}
-          </span>
-        }
-        <h2>Sign up</h2>
-        <div className={styles.form_group.concat(' ', errors.email ? styles.hasError : '')}>
-          <FormInput
-            type="text"
-            placeholder="email"
-            name="email"
-            autoComplete="email"
-            register={register}
-            errors={errors.email?.message}
-            rules={{
-              required: c.en.MESSAGES.errors.required,
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: c.en.MESSAGES.errors.emailFormat,
-              },
-            }}
-          />
-        </div>
-        <div className={styles.form_group.concat(' ', errors.password ? styles.hasError : '')}>
-          <FormInput
-            name="password"
-            placeholder={'Password'}
-            register={register}
-            type="password"
-            autoComplete="current-password"
-            errors={errors.password?.message}
-            rules={{
-              required: c.en.MESSAGES.errors.required,
-              minLength: {
-                value: 8,
-                message: c.en.MESSAGES.errors.minLength,
-              },
-              validate: passwordValidation,
-            }}
-          />
-        </div>
-        <div className={styles.form_group.concat(' ', errors.cPassword ? styles.hasError : '')}>
-          <FormInput
-            name="cPassword"
-            register={register}
-            errors={errors.cPassword?.message}
-            rules={{
-              required: c.en.MESSAGES.errors.required,
-              validate: passwordCompare,
-            }}
-            type="password"
-            placeholder="Confirm password"
-            autoComplete="repeat-password"
-          />
-        </div>
-        <div className={styles.form_group}>
-          <Button
-            title={t('auth.signup')}
-            type="submit"
-            clickHandler={() => {
-              clearErrors();
-            }}
-          />
-          or
-          <Link to="/login">{t('auth.signin')}</Link>
-        </div>
-      </form>
+      <ErrorBoundary>
+        <form
+          className={styles.signup_form.concat(' ', errors.form ? styles.hasError : '')}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {
+            <span role="alert" className={styles.error}>
+              {errors.form?.message}
+            </span>
+          }
+          <h2>Sign up</h2>
+          <div className={styles.form_group.concat(' ', errors.email ? styles.hasError : '')}>
+            <FormInput
+              type="text"
+              placeholder="email"
+              name="email"
+              autoComplete="email"
+              register={register}
+              errors={errors.email?.message}
+              rules={{
+                required: c.en.MESSAGES.errors.required,
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: c.en.MESSAGES.errors.emailFormat,
+                },
+              }}
+            />
+          </div>
+          <div className={styles.form_group.concat(' ', errors.password ? styles.hasError : '')}>
+            <FormInput
+              name="password"
+              placeholder={'Password'}
+              register={register}
+              type="password"
+              autoComplete="current-password"
+              errors={errors.password?.message}
+              rules={{
+                required: c.en.MESSAGES.errors.required,
+                minLength: {
+                  value: 8,
+                  message: c.en.MESSAGES.errors.minLength,
+                },
+                validate: passwordValidation,
+              }}
+            />
+          </div>
+          <div className={styles.form_group.concat(' ', errors.cPassword ? styles.hasError : '')}>
+            <FormInput
+              name="cPassword"
+              register={register}
+              errors={errors.cPassword?.message}
+              rules={{
+                required: c.en.MESSAGES.errors.required,
+                validate: passwordCompare,
+              }}
+              type="password"
+              placeholder="Confirm password"
+              autoComplete="repeat-password"
+            />
+          </div>
+          <div className={styles.form_group}>
+            <Button
+              title={t('auth.signup')}
+              type="submit"
+              clickHandler={() => {
+                clearErrors();
+              }}
+            />
+            or
+            <Link to="/login">{t('auth.signin')}</Link>
+          </div>
+        </form>
+      </ErrorBoundary>
     </div>
   );
 };
