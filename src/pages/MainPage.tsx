1f0c8bc6b1b79f284a73a7a00441d9369cd57cbf
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-22 16:58:49 +0500

    fix: add missing dependencies, correct main page layout

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,37 +1,22 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
-import { Header } from '../components/Header/Header';
-// import { useTranslation } from 'react-i18next';
-import Request from '../components/Editor/Request';
-import Response from '../components/Editor/Response';
-import Variables from '../components/Editor/Variables';
-import Headers from '../components/Editor/Headers';
 import { Suspense, useState } from 'react';
 import styles from '../components/Editor/Editor.module.css';
 import { LazyDoc } from '../components/Editor';
 import { Sidebar } from '../components/Editor/Sidebar';
+import GraphqlEditor from '../components/GraphqlEditor/GraphqlEditor';
+import { TabBlock } from '../components/TabBlock/TabBlock';
 
 const MainPage = () => {
-  // const { t } = useTranslation();
-  const { isAuth } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
 
   function openDoc() {
     setIsOpen(!isOpen);
   }
 
-  return !isAuth ? (
+  return (
     <>
-      <Header />
       <div className={styles.wrapper}>
-        <div className={styles.editor_container}>
-          <Request />
-          <Response />
-        </div>
-        <div>
-          <Variables />
-          <Headers />
-        </div>
+        <GraphqlEditor />
+        <TabBlock />
       </div>
       <div className={styles.interface}>
         <Sidebar docHandler={() => openDoc()} />
@@ -40,8 +25,6 @@ const MainPage = () => {
         </Suspense>
       </div>
     </>
-  ) : (
-    <Navigate to="/login" />
   );
 };
 
