8fb54d43ba3009c8b9204c93b63762514d76b697
Stanislav Mamulin <StanislavPM@ya.ru>
2023-05-27 19:04:38 +0500

    feat: add persistence of application state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './slices/userSlice';
 import requestReducer from './slices/requestParametersSlice';
 import notificationsReducer from './slices/notificationsSlice';
 import { rickAndMortyApi } from '../services/rickAndMortyAPI';
 import { tokenExpirationMiddleware } from './middlewares/tokenExpirationMiddleware';
+
+const persistConfig = {
+  key: 'graphql',
+  storage,
+  whitelist: ['user', 'requestParameters'],
+  blacklist: ['notifications'],
+};
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -14,11 +23,14 @@ const rootReducer = combineReducers({
   notifications: notificationsReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([rickAndMortyApi.middleware, tokenExpirationMiddleware]),
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
